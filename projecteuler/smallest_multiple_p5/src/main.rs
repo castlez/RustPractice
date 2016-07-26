40a2ee031c9ccdb45061beb95724f1279df641e7
castle <jcastle@tripwire.com>
2016-07-26 14:26:57 -0700

    tidy up comments in pe5

--- projecteuler/smallest_multiple_p5/src/main.rs
+++ projecteuler/smallest_multiple_p5/src/main.rs
@@ -1,8 +1,9 @@
-//solution to Project Euler problem 5
+// Solution to Project Euler problem 5
 
-//global input
+///Global input
 const A:[i64;20] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
+///Main function
 fn main() {
     let first_result = first_attempt();
     println!("First attempt result = {}",first_result);
@@ -11,9 +12,9 @@ fn main() {
     println!("Second attempt result = {}",second_result);
 }
 
-// first attempt (fails)
-//inrementally multiples the primes between 1 and 20
-//together and checks to see if any of the results are solutions
+///First attempt (fails)
+// Incrementally multiples the primes between 1 and 20
+// together and checks to see if any of the results are solutions
 fn first_attempt() -> i64 {
     let pa:[i64;8] = [2,3,5,7,11,13,17,19]; //primes in A discluding 1 (which is init of 'cur')
 
@@ -32,11 +33,11 @@ fn first_attempt() -> i64 {
     return -1; //failed to find a potential solution
 }
 
-//second attempt (fails)
-//If the numbers in rm are divisors of the number
-//than all other numbers in range 1-20 will be as well.
-//Using the same algorith as first attempt, but with rm 
-//instead of pa
+///Second attempt (fails)
+// If the numbers in rm are divisors of the number
+// than all other numbers in range 1-20 will be as well.
+// Using the same algorith as first attempt, but with rm 
+// instead of pa
 fn take_two() -> i64 {
     let rm:[i64;8] = [13,14,15,16,17,18,19,20]; //required multiples, the rest of the numbers are trivially included
 
@@ -56,7 +57,7 @@ fn take_two() -> i64 {
 }
 
 
-//check if a number is a multiple of 1-20
+///Check if a number is a multiple of 1-20
 fn check_num(check:i64)->bool{
     for i in 1..21 {
         if check%i != 0 { //check if it's evenly divisible (no remainder)
